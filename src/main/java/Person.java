cc6cec1d266c88e691835da290cab47ce63bde47
GeorgeMoroz1 <yura0991@mail.ru>
2023-02-10 21:25:57 +0300

    Initial commit

---
+++ src/main/java/Person.java
@@ -0,0 +1,77 @@
+import java.util.OptionalInt;
+
+public class Person {
+    protected final String name;
+    protected final String surname;
+    protected int age;
+    protected String address;
+
+
+    public Person(PersonBuilder personBuilder) {
+        this.name = personBuilder.getName();
+        this.surname = personBuilder.getSurname();
+        setAge(personBuilder.getAge());
+        this.address = personBuilder.getAddress();
+    }
+
+    public boolean hasAge() {
+        return age > 0;
+    }
+
+    public boolean hasAddress() {
+        return !address.equals(null);
+    }
+    public void setAge(int age) {
+        // проверяем задан ли возраст
+        if (hasAge()) {
+            throw new IllegalStateException("Возраст уже задан!");
+        }
+
+        if (age < 0 || age > 100) {
+            throw new IllegalArgumentException("Возраст указан некорректно!");
+        } else {
+            this.age = age;
+        }
+    }
+    public String getName() {
+        return name;
+    }
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void happyBirthday() {
+        age++;
+    }
+    public PersonBuilder newChildBuilder() {
+        return new PersonBuilder().setSurname(this.surname).setAddress(this.address).setAge(age);
+    }
+
+
+    @Override
+    public String toString() {
+        return "[" + name + ", " + surname + ", " + age + ", " + address + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((surname == null) ? 0 : surname.hashCode());
+        return result;
+    }
+}
+
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,40 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+Person person = new PersonBuilder().setName("Юрий").setSurname("Морозов").setAge(32).setAddress("Санкт-Петербург").build();
+    @BeforeEach
+    public void beforeTest() {
+        System.out.println("Подготовка к тесту");
+    }
+
+    @Test
+    public void noName() {
+        Assertions.assertNotNull(person.name);
+    }
+    @Test
+    public void noSurname() {
+        Assertions.assertNotNull(person.surname);
+    }
+    @Test
+    public void ageTest() {
+        Assertions.assertTrue(person.hasAge());
+        Assertions.assertFalse(person.getAge()>=-100 && person.getAge() < 0);
+    }
+
+    @Test
+    public void ChildTest() {
+        Person person = new PersonBuilder().setName("Юрий").setSurname("Морозов").setAge(32).setAddress("Санкт-Петербург").build();
+        Person son = person.newChildBuilder().setName("Иван").setSurname("Морозов").setAge(11).setAddress("Санкт-Петербург").build();
+        Assertions.assertEquals(person.getSurname(), son.getSurname());
+        Assertions.assertEquals(person.getAddress(), son.getAddress());
+    }
+    @AfterEach
+    public void afterTest() {
+        System.out.println("Тест завершён");
+    }
+}
